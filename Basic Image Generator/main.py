45952995f4f84343241db7402e1060c6ae6e7b5d
Jakub <jd.kuba96@gmail.com>
2020-10-11 20:19:21 +0200

    Added training code and succesfully trained Gan network results from 25 epochs are in results folder

--- Basic Image Generator/main.py
+++ Basic Image Generator/main.py
@@ -4,57 +4,49 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
-import torchvision.datasets as dset 
+import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
 
 # Hyperparameters
-batch_size = 64
-image_size = 64
+batchSize = 64 
+imageSize = 64 
 
 # Data Preparation
-transform = transforms.Compose([transforms.Scale(image_size), 
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), )])
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) 
 
-data_set = dset.CIFAR10(root='./data', download=True, transform=transform)
-data_loader = torch.utils.data.DataLoader(data_set, 
-                                          batch_size=batch_size, 
-                                          shuffle = True, 
-                                          num_workers=2)
+dataset = dset.CIFAR10(root = './data', download = True, transform = transform) 
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 0) 
 
-# Weights initialization takes as input neural network m
+
 def weights_init(m):
-    class_name = m.__class__.__name__
-    if class_name.find('Conv') != -1:
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
-    elif class_name.find('BatchNorm') != -1:
+    elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
 # Generator
 class G(nn.Module):
+
     def __init__(self):
         super(G, self).__init__()
         self.main = nn.Sequential(
-            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias = False),
             nn.BatchNorm2d(512),
             nn.ReLU(True),
-
-            nn.ConvTranspose2d(512, 256, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias = False),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
-
-            nn.ConvTranspose2d(256, 128, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(512),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(128),
             nn.ReLU(True),
-
-            nn.ConvTranspose2d(128, 64, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias = False),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
-
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias = False),
             nn.Tanh()
         )
 
@@ -62,7 +54,7 @@ class G(nn.Module):
         output = self.main(input)
         return output
 
-netG = G() # Creating Generator object
+netG = G()  # Creating Generator object
 netG.apply(weights_init)
 
 # Discriminator
@@ -71,27 +63,70 @@ class D(nn.Module):
     def __init__(self):
         super(D, self).__init__()
         self.main = nn.Sequential(
-            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(128)
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.Conv2d(3, 64, 4, 2, 1, bias = False),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(64, 128, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(128, 256, 4, 2, 1, bias = False),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(256, 512, 4, 2, 1, bias = False),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(512, 1, 4, 1, 0, bias=False)
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(512, 1, 4, 1, 0, bias = False),
             nn.Sigmoid()
         )
+
     def forward(self, input):
         output = self.main(input)
         return output.view(-1)
 
-netD = D() # Creating discriminator object
+netD = D() # Creating the discriminator
 netD.apply(weights_init)
+
+criterion = nn.BCELoss()
+optimizerD = optim.Adam(netD.parameters(), lr = 0.0002, betas = (0.5, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr = 0.0002, betas = (0.5, 0.999))
+
+for epoch in range(25):
+
+    for i, data in enumerate(dataloader, 0):
+        
+        # Updating the weights of the neural network of the discriminator
+
+        netD.zero_grad()
+        
+        # Training the discriminator with a real image of the dataset
+        real, _ = data
+        input = Variable(real)
+        target = Variable(torch.ones(input.size()[0]))
+        output = netD(input)
+        errD_real = criterion(output, target)
+        
+        # Training the discriminator with a fake image generated by the generator
+        noise = Variable(torch.randn(input.size()[0], 100, 1, 1))
+        fake = netG(noise)
+        target = Variable(torch.zeros(input.size()[0]))
+        output = netD(fake.detach())
+        errD_fake = criterion(output, target)
+        
+        # Backpropagating the total error
+        errD = errD_real + errD_fake
+        errD.backward()
+        optimizerD.step()
+
+        # Updating the weights of the neural network of the generator
+
+        netG.zero_grad()
+        target = Variable(torch.ones(input.size()[0]))
+        output = netD(fake)
+        errG = criterion(output, target)
+        errG.backward()
+        optimizerG.step()
+        
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f' % (epoch, 25, i, len(dataloader), errD.item(), errG.item()))
+        if i % 100 == 0:
+            vutils.save_image(real, '%s/real_samples.png' % "./results", normalize = True)
+            fake = netG(noise)
+            vutils.save_image(fake.data, '%s/fake_samples_epoch_%03d.png' % ("./results", epoch), normalize = True)
--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 In this repository i will be collecting most of my computer vision projects
 
-<details><summary>Basic Image Generator</summary>
+<details><summary>Basic Image Generator Python</summary>
 <p>
 Basic image generator using GAN network and CIFAR-10 dataset.
 
 <a href="http://www.cs.toronto.edu/~kriz/cifar.html">DATA SET</a>
 
+torchvision == 0.1.9 needed 
 </p>
 </details>
 
-<details><summary>Dog Cat image recognition</summary>
+<details><summary>Dog Cat image recognition Jupyter Notebook</summary>
 <p>
 Small Project when i used basic CNN to distinguish between cats and dogs 
 
