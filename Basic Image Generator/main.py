2c4d59f65351fadfedcb1d9923c9a62f30804376
Jakub <jd.kuba96@gmail.com>
2020-10-11 11:03:42 +0200

    Data Preparation, downloading and loading CIFAR 10 dataset

--- Basic Image Generator/main.py
+++ Basic Image Generator/main.py
@@ -12,3 +12,14 @@ from torch.autograd import Variable
 # Hyperparameters
 batch_size = 64
 image_size = 64
+
+# Data Preparation
+transform = transforms.Compose([transforms.Scale(image_size), 
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), )])
+
+data_set = dset.CIFAR10(root='./data', download=True, transform=transform)
+data_loader = torch.utils.data.DataLoader(data_set, 
+                                          batch_size=batch_size, 
+                                          shuffle = True, 
+                                          num_workers=2)
